c760c5d2df663e687c80359e920b2ed9d0b7f92d
KT 2022 <103476893+kt946@users.noreply.github.com>
2022-06-04 22:06:40 -0400

    add ability to save to local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,6 @@
+// let code load HTML/CSS and timeblocks first
 var currentDate = moment().format('dddd, MMMM Do');
+var auditTimer = "";
 
 // display date at top of page
 $("#currentDay").append(currentDate);
@@ -10,13 +12,14 @@ var loadTimeBlocks = function() {
         // create label for each row by converting 24-hour time into 12-hour time format
         var timeLabel = moment(i, "H").format("hA");
         $(".container").append(
-            "<div class='row time-block'><div class='col hour card-header'><span>" + timeLabel + "</span></div>"
+            "<div class='row time-block'><div class='col hour card-header'><span>"
+             + timeLabel + "</span></div>"
         );
     }
     // add textarea and button to each row
     $(".row").append(
         "<textarea class='col-10 description'></textarea>" +
-        "<div class='col saveBtn'><i type='button' class='fas fa-save'></i></div>"
+        "<button class='col saveBtn'><i type='button' class='fas fa-save'></i></button>"
     );
 };
 
@@ -45,14 +48,24 @@ var auditTimeBlock = function() {
     }
 };
 
+// ready once document finishes loading HTML/CSS and timeblocks
+$(document).ready(function () {
+    $(".saveBtn").on("click", function() {
+        var rowTimeId = $(this).parent().children().find("span").text();
+        var rowText = $(this).siblings("textarea").val();
+    
+        localStorage.setItem(rowTimeId, rowText);
+    });
+});
+
 // interval for checking timeblocks
 var timer = function() {
     // run function once at start of hour
     auditTimeBlock();
     // set interval for every hour
-    var auditTimer = setInterval(auditTimeBlock, (1000*60)*60);
+    auditTimer = setInterval(auditTimeBlock, (1000*60)*60);
 };
-// variable to check how much time is left until next hour
+// variable to check how much time is left until next hour in milliseconds
 var timeLeft = moment().add(1, 'h').minute(0).second(0).millisecond(0).diff(moment());
 // timeout interval set to start upon next hour
 setTimeout(timer, timeLeft);
